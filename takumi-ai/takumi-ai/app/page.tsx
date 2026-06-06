1781d0795f960cdf9c41b67af55fd85f2dbbfa30
takumi ito <tyidong215@icloud.com>
2026-06-06 19:29:06 +0900

    送信機能と会話履歴表示を追加

--- takumi-ai/takumi-ai/app/page.tsx
+++ takumi-ai/takumi-ai/app/page.tsx
@@ -1,15 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const[message,setMessage] = useState("");
+  const[messages,setMessages] = useState<string[]>([]);
+
+  function handleSend(){
+    setMessages([...messages,message]);
+    setMessage("");
+  }
   return (
     <main className="bg-black text-white h-screen">
       <h1>Takumi AI</h1>
 
       <div className="bg-gray-800 h-96 border border-gray-700 rounded-lg w-full max-w-3xl">
         <p>質問を入力してください</p>
+        {messages.map((item,index) => (
+          <p key={index}>あなた；{item}</p>
+        ))}
       </div>
 
-      <input />
+      <input 
+      value={message}
+      onChange={(e) => setMessage(e.target.value)}
+      className="border border-white"
+      />
+      <p>{message}</p>
 
-      <button>送信</button>
+      <button onClick={handleSend}>
+        送信
+        </button>
 
     </main>
   );
