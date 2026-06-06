7bba2db9f864dce8d81633f8258a73d67e1fcb14
takumi ito <tyidong215@icloud.com>
2026-06-06 17:27:47 +0900

    チャット画面の土台を完成

---
+++ takumi-ai/takumi-ai/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white h-screen">
+      <h1>Takumi AI</h1>
+
+      <div className="bg-gray-800 h-96 border border-gray-700 rounded-lg w-full max-w-3xl">
+        <p>質問を入力してください</p>
+      </div>
+
+      <input />
+
+      <button>送信</button>
+
+    </main>
+  );
+}
